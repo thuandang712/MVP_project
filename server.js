2cd49a354fd31c92a63717cdf4df0b0dc1695c70
Thuan Dang <thuandang@Thuans-Mac-mini.local>
2021-08-02 15:24:05 -0500

    2nd commit

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+// dependencies
 const express = require('express');
 const app = express();
+const path = require('path')
 
+// port
 const PORT = process.env.PORT || 4000;
 
-app.use(express.static('public'))
+// link to FE
+app.use(express.static(path.join(__dirname, 'public')));
 
+// handle routes
+    
+
+
+
+
+
+// listen on port
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
 })
