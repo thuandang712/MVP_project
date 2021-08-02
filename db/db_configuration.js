18c09191a5ad77040abda8c1eba73aaa035fe01b
Thuan Dang <thuandang@Thuans-Mac-mini.local>
2021-08-02 16:06:25 -0500

    init deploy

--- db/db_configuration.js
+++ db/db_configuration.js
@@ -1,14 +1,13 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    // connectionString: process.env.DATABASE_URL,
-    // ssl: {
-    //   rejectUnauthorized: false
-    // }
-    user: 'thuandang', // TODO/EXAMPLE: REPLACED WITH PLACEHOLDER
-    database: 'mvp_project',  // TODO: Replace process.env.DATABASE_NAME
-    password: '',// TODO: Replace process.env.DATABASE_PASSWORD
-
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+    // user: 'thuandang', // TODO/EXAMPLE: REPLACED WITH PLACEHOLDER
+    // database: 'mvp_project',  // TODO: Replace process.env.DATABASE_NAME
+    // password: '',// TODO: Replace process.env.DATABASE_PASSWORD
   });
 
 module.exports = pool
