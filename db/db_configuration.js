2d3362287cf63a07d2a66e32778d5bd32be87f19
Thuan Dang <thuandang@Thuans-Mac-mini.local>
2021-08-04 12:37:50 -0500

    finished DB CRUD on FE

--- db/db_configuration.js
+++ db/db_configuration.js
@@ -1,17 +1,16 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    // connectionString: process.env.DATABASE_URL,
-    // host: 'https://dang-mvp-project.herokuapp.com/'
-    // ssl: {
-    //   rejectUnauthorized: false
-    // }
-
+    connectionString: process.env.DATABASE_URL,
+    host: 'https://dang-mvp-project.herokuapp.com/',
+    ssl: {
+      rejectUnauthorized: false
+    }
     // for local env
-    user: 'thuandang', // TODO/EXAMPLE: REPLACED WITH PLACEHOLDER
-    database: 'mvp_project',  // TODO: Replace process.env.DATABASE_NAME
-    host: 'localhost',
-    password: '',// TODO: Replace process.env.DATABASE_PASSWORD
+    // user: 'thuandang', 
+    // database: 'mvp_project', 
+    // host: 'localhost',
+    // password: '',
   });
 
 module.exports = pool
